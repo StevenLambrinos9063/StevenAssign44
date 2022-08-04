19a0158597b753831aefeb78231cab1ee633448a
StevenLambrinos9063 <StevenLambrinos9063>
2022-08-04 16:13:13 -0700

    Twelve Commit
    The settings page which is under construction

---
+++ StevenAssign44/src/main/java/steven/lambrinos/n01429063/SettingsScreen.java
@@ -0,0 +1,67 @@
+//Steven Lambrinos N01429063 RNA
+package steven.lambrinos.n01429063;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SettingsScreen#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SettingsScreen extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SettingsScreen() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment SettingsScreen.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SettingsScreen newInstance() {
+        SettingsScreen fragment = new SettingsScreen();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.settings_screen, container, false);
+
+        return view;
+    }
+}
